093609ca09ddfc32a16576842540669333084b8e
johnny shafir <97397410+Josnon@users.noreply.github.com>
2022-12-23 10:47:40 +0200

    Starter

---
+++ index.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+//!Blocking , Synchoronus way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+//!Non-Blocking , asynchronous way
+// fs.readFile("./txt/staart.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR! ♣");
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile(
+//         `./txt/final.txt`,
+//         `${data2}\n${data3}`,
+//         "utf-8",
+//         (err) => {}
+//       );
+//     });
+//   });
+// });
+// console.log("will read file!");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("this is the OVERVIEW!");
+  } else if (pathName === "/product") {
+    res.end("this is the PRODUCT!");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listing to requests on port 8000");
+});
